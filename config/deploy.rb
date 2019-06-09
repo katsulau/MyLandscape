d3ba09d5d04bd22d75b7a02ec17584f466fcaaa4
katsulau <efthnyb.1994@gmail.com>
2019-06-09 15:25:57 +0900

    repo_urlの修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.11.0"
 
 set :application, "MyLandscape"
-set :repo_url, "git@example.com:katsulau/MyLandscape.git"
+set :repo_url, "git@github.com:katsulau/MyLandscape.git"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
