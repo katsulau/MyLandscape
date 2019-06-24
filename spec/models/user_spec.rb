3520f212b0201b36780da90c2d32b054f3fe29a1
katsulau <efthnyb.1994@gmail.com>
2019-06-24 23:16:34 +0900

    Add: テストを追加
    ・ユーザー登録機能
    　メールアドレスが重複していると登録できないことを確認

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -30,5 +30,12 @@ describe User do
       user.valid?
       expect(user.errors[:password_confirmation]).to include("doesn't match Password")
     end
+
+    it "is invalid with a duplicate email address" do
+      user = create(:user)
+      another_user = build(:user, email: user.email)
+      another_user.valid?
+      expect(another_user.errors[:email]).to include("has already been taken")
+    end
   end
 end
