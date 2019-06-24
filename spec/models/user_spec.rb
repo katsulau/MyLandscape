380df866f04b8fb222c1f06af3f1abd4478e8203
katsulau <efthnyb.1994@gmail.com>
2019-06-24 22:48:31 +0900

    Add: テストを追加
    ・ユーザー登録機能
     passwordを入力していないと保存できないことを確認

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,5 +12,11 @@ describe User do
       user.valid?
       expect(user.errors[:name]).to include("can't be blank")
     end
+
+    it "is invalid without a email" do
+      user = build(:user, email: nil)
+      user.valid?
+      expect(user.errors[:email]).to include("can't be blank")
+    end
   end
 end
