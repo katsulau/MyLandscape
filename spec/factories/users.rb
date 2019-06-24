c6652ac246af02f10fb5ce1db73917a66653dd75
katsulau <efthnyb.1994@gmail.com>
2019-06-24 18:36:55 +0900

    Make: FactoryBotを利用したインスタンスを記述

---
+++ spec/factories/users.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+
+  factory :user do
+    name                  {"testname"}
+    image                 {"xxxx.jpg"}
+    email                 {"test@gmail.com"}
+    password              {"00000000"}
+    password_confirmation {"00000000"}
+  end
+
+end
