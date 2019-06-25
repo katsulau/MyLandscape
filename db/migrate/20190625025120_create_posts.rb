9514ff894958c3854cd065f8a54a421af29490d8
katsulau <efthnyb.1994@gmail.com>
2019-06-25 12:45:37 +0900

    Add: postテーブルのname、imageカラムにnull: falseを追加
    rake db:rollbackを行い、
    Nameカラム及びimageカラムにnull: falseを追加

--- db/migrate/20190625025120_create_posts.rb
+++ db/migrate/20190625025120_create_posts.rb
@@ -1,8 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
-      t.string       :name
-      t.string       :image
+      t.string       :name, null: false
+      t.string       :image, null: false
       t.text         :description
       t.references   :user, foreign_key: true
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,8 @@
 ActiveRecord::Schema.define(version: 2019_06_25_025120) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
-    t.string "image"
+    t.string "name", null: false
+    t.string "image", null: false
     t.text "description"
     t.bigint "user_id"
     t.datetime "created_at", null: false
