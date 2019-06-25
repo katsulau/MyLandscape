52e6fe4b5b2d01c65618f2560a9f9f5068c8fb41
katsulau <efthnyb.1994@gmail.com>
2019-06-25 19:22:25 +0900

    Add: 単体テストを追加
    下記２点を確認。
    ・createアクションが動いた時にデータベースに保存される
    ・posts#indexにリダイレクトする

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,10 +1,29 @@
 require 'rails_helper'
 
-describe PostsController do
+describe PostsController, type: :controller  do
+  let(:user) { create(:user) }
+
   describe 'GET #new' do
     it "renders the :new template" do
       get :new
       expect(response).to render_template :new
+    end
+  end
+
+  describe 'POST #create' do
+    before do
+      login_user user
+    end
+
+    it "saves the new post in the database" do
+      expect do
+        post :create, params: { post: attributes_for(:post) }
+      end.to change(Post, :count).by(1)
+    end
+
+    it "redirects to posts#index" do
+      post :create, params: { post: attributes_for(:post) }
+      expect(response).to redirect_to posts_path
     end
   end
 end
