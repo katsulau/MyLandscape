64cc35c6230e03b63deec1b00da88a6f31fdeb69
katsulau <efthnyb.1994@gmail.com>
2019-06-29 16:13:19 +0900

    Add: テストを追加
    以下の点についてテストを行った
    
    ・ログイン状態でテキストがあれば登録できる
    ・投稿したユーザー以外のユーザーでもコメントが投稿できる
    ・テキストがないと投稿できない
    ・ログインをしていないと投稿できない

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+  let(:another_user) {create(:user) }
+  let(:post) { create(:post, user_id: user.id) }
+
+  describe "#create" do
+    it "is valid with text" do
+      comment = build(:comment, user_id: user.id, post_id: post.id)
+      expect(comment).to be_valid
+    end
+
+    it "is valid if an another_user comments" do
+      comment = build(:comment, user_id: another_user.id, post_id: post.id)
+      expect(comment).to be_valid
+    end
+
+    it "is invalid without text" do
+      comment = build(:comment, text: "", user_id: user.id, post_id: post.id)
+      comment.valid?
+      expect(comment.errors[:text]).to include("can't be blank")
+    end
+
+    it "is invalid without user_id" do
+      comment = build(:comment, post_id: post.id)
+      comment.valid?
+      expect(comment.errors[:user]).to include("must exist")
+    end
+  end
 end
