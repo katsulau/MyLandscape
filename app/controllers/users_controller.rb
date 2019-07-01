5b9c19fa4736994f6f81b218cee481846172bc6f
katsulau <efthnyb.1994@gmail.com>
2019-07-02 00:10:22 +0900

    Fix: @userをメソッドで共通化

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,24 @@
 class UsersController < ApplicationController
 
+  before_action :set_user, only: [:edit, :show, :update]
+
   def edit
-    @user = User.find(params[:id])
   end
 
   def show
-    @user = User.find(params[:id])
     @posts = @user.posts.page(params[:page]).per(6)
   end
 
   def update
-    user = User.find(params[:id])
-      user.update(user_params)
+      @user.update(user_params)
       redirect_to user_path(params[:id])
   end
 
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:name, :image)
