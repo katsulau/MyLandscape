b36198ce2b693e72697685cb193c9c651163144e
katsulau <efthnyb.1994@gmail.com>
2019-07-01 23:39:31 +0900

    Add: users#updateを追加
    Updateアクションを定義し、
    ユーザー情報の変更が保存されるよう記述

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,16 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @posts = @user.posts.page(params[:page]).per(6)
   end
+
+  def update
+    user = User.find(params[:id])
+      user.update(user_params)
+      redirect_to user_path(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :image)
+  end
 end
