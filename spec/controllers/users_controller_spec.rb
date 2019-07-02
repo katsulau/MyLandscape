0e20ce33616d0ac88f72ef53c6f5a483c0fc31f6
katsulau <efthnyb.1994@gmail.com>
2019-07-02 12:33:06 +0900

    Add: Rspecテスト追加
    ・users#updateのテストを追加
    
    以下の点について確認した。
    ・リクエストした際に想定したuserが呼び出されること。
    ・userの属性を変更し更新できること。
    ・更新後はユーザマイページに遷移すること。

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -22,4 +22,34 @@ RSpec.describe UsersController, type: :controller do
       expect(response).to render_template :show
     end
   end
+
+  describe 'GET #edit' do
+    it "assigns the requested user to @user" do
+      get :edit, params: { id: user.id }
+      expect(assigns(:user)).to eq user
+    end
+
+    it "renders the :edit template" do
+      get :edit, params: { id: user.id }
+      expect(response).to render_template :edit
+    end
+  end
+
+  describe 'PATCH #update' do
+    it "locates the requersted @user" do
+      patch :update, params: { user: attributes_for(:user), id: user.id }
+      expect(assigns(:user)).to eq user
+    end
+
+    it "changes @user's attributes" do
+      patch :update, params: {id: user.id, user: attributes_for(:user, name: 'abc')}
+      user.reload
+      expect(user.name).to eq("abc")
+    end
+
+    it "redirects to user_path" do
+      patch :update, params: { user: attributes_for(:user), id: user.id }
+      expect(response).to redirect_to user_path(user)
+    end
+  end
 end
