ab64a0479be491e59aff66703dd80a8231a61102
katsulau <efthnyb.1994@gmail.com>
2019-07-03 18:21:25 +0900

    Change: 画像のアップロード先をS3に変更

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,17 @@
 require 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
+
 CarrierWave.configure do |config|
   config.storage = :fog
-  config.fog_provider = 'fog/google'
+  config.fog_provider = 'fog/aws'
   config.fog_credentials = {
-      :provider                         => 'Google',
-      :google_storage_access_key_id     => ENV["GOOGLE_STORAGE_ACCESS_KEY_ID"],
-      :google_storage_secret_access_key => ENV["GOOGLE_STORAGE_SECRET_ACCESS_KEY"]
-      }
-      config.fog_directory = 'my-landscape'
+    provider: 'AWS',
+    aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+    region: 'ap-northeast-1'
+  }
+
+  config.fog_directory  = 'mylandscape'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/mylandscape'
 end
