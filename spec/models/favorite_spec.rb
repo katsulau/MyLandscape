f47741bcd399150f3021ff8170d5257fd31ea53c
katsulau <efthnyb.1994@gmail.com>
2019-07-05 00:09:46 +0900

    Add: テストを追加
    Favoriteモデルのテストを記述
    
    以下の項目について確認した。
    
    ・user_idとpost_idがあればレコードが保存されること。
    ・user_idがなければレコードが保存されないこと。
    ・post_idがなければレコードが保存されないこと。

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Favorite, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:user) { create(:user) }
+  let(:post) { create(:post, user_id: user.id) }
+
+  describe "#create" do
+    it "is valid with user_id and post_id" do
+      favorite = Favorite.create(user_id: user.id, post_id: post.id)
+      expect(favorite).to be_valid
+    end
+
+    it "is invalid without user_id " do
+      favorite = Favorite.create(user_id: nil, post_id: post.id)
+      favorite.valid?
+      expect(favorite.errors[:user]).to include("must exist")
+    end
+
+    it "is invalid without post_id " do
+      favorite = Favorite.create(user_id: user.id, post_id: nil)
+      favorite.valid?
+      expect(favorite.errors[:post]).to include("must exist")
+    end
+  end
 end
