e5d382ee3e72559314c62768b2ca60ae53512e16
katsulau <efthnyb.1994@gmail.com>
2019-07-04 16:44:38 +0900

    Add: テストを追加
    posts#updateのテストを追加
    
    以下の項目を確認
    ・posts#updateが動いた時、@postに期待した値が代入されている。
    ・postのデータを変更した際に、DBに変更が反映されている。
    ・posts#updateが動いた後、post_pathへリダイレクトする。

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -72,6 +72,27 @@ describe PostsController, type: :controller  do
     end
   end
 
+  describe 'PATCH #update' do
+    let(:post) { create(:post, user_id: user.id)}
 
+    before do
+      login_user user
+    end
 
+    it "locates the requersted @post" do
+      patch :update, params: { post: attributes_for(:post), id: post.id }
+      expect(assigns(:post)).to eq post
+    end
+
+    it "changes @post's attributes" do
+      patch :update, params: {id: post.id, post: attributes_for(:post, name: 'abcde')}
+      post.reload
+      expect(post.name).to eq("abcde")
+    end
+
+    it "redirects to post_path" do
+      patch :update, params: { post: attributes_for(:post), id: post.id }
+      expect(response).to redirect_to post_path(post)
+    end
+  end
 end
