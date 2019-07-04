e30b9bcf8ca669db64ecdae4d299dfa2eb76f484
katsulau <efthnyb.1994@gmail.com>
2019-07-04 20:13:09 +0900

    Add: destroyアクションを追加

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,11 +1,20 @@
 class FavoritesController < ApplicationController
 
+  before_action :set_post
+
   def create
     @favorite = current_user.favorites.create(post_id: params[:post_id])
+    redirect_to post_path(@post)
   end
 
 
   def destroy
+    @favorite = Favorite.find_by(post_id: params[:post_id], user_id: current_user.id)
+    @favorite.destroy
+    redirect_to post_path(@post)
+  end
 
+  def set_post
+    @post = Post.find(params[:post_id])
   end
 end
