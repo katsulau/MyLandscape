2bbaf5f4d263e475e80191ec86097e1af27572b8
katsulau <efthnyb.1994@gmail.com>
2019-07-06 16:31:56 +0900

    Add: 多言語対応のルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :users, only:[:edit, :update, :show]
+  scope '(:locale)', locale: /#{I18n.available_locales.map(&:to_s).join('|')}/ do
+  # For details on the DSL available within this file, see
 
-  resources :posts, only:[:index, :new, :create, :edit, :update, :show] do
-    resources :comments, only:[:create]
-    resources :favorites, only: [:create, :destroy]
+    devise_for :users
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+    resources :users, only:[:edit, :update, :show]
+
+    resources :posts, only:[:index, :new, :create, :edit, :update, :show] do
+      resources :comments, only:[:create]
+      resources :favorites, only: [:create, :destroy]
+    end
+
+    root "toppages#index"
   end
-
-  root "toppages#index"
 end
