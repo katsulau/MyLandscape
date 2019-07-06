bf916b8b9b34654e0c29dcc7f6747da4f0d4f889
katsulau <efthnyb.1994@gmail.com>
2019-07-06 16:31:14 +0900

    Add: 動的に言語を切り替えるための設定を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,25 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  before_action :set_locale
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :image])
   end
 
   def after_sign_in_path_for(resource)
     posts_path
+  end
+
+  def set_locale
+      I18n.locale = locale
+  end
+
+  def locale
+      @locale ||= params[:locale] ||= I18n.default_locale
+  end
+
+  def default_url_options(options={})
+      options.merge(locale: locale)
   end
 end
