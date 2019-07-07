7de67a64617b63955d20c2fff109cbd5316d8599
katsulau <efthnyb.1994@gmail.com>
2019-07-07 15:33:14 +0900

    Delete: requireの記述を削除

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,7 +8,7 @@ require 'rspec/rails'
 require 'devise'
 require File.expand_path("spec/support/controller_macros.rb")
 require 'shoulda/matchers'
-require I18n.locale = :en
+I18n.locale = :en
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
