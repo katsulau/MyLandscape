fa01a96fd2536db560ab309d7b733473c584501f
katsulau <efthnyb.1994@gmail.com>
2019-07-13 14:59:01 +0900

    Fix: Validatesの記述を修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   mount_uploader :image, ImageUploader
-  validates :name, presence: :true
-  validates :image, presence: :true
+  validates :name, presence: true
+  validates :image, presence: true
 
   belongs_to :user
   has_many :comments
